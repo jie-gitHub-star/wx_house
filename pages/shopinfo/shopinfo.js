e0b4fd65a647eb5359e8bb382b82b47e189f847f
zhijie032 <1752983361@qq.com>
2019-11-15 11:49:00 +0800

    Initial Commit

---
+++ pages/shopinfo/shopinfo.js
@@ -0,0 +1,75 @@
+// pages/shopinfo/shopinfo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    slider: [{src: "../../icon/upload/avatar_10.png"}
+    , { src: "../../icon/upload/avatar_11.png"}
+    , { src: "../../icon/upload/avatar_12.png"}
+    , { src: "../../icon/upload/avatar_13.png"}
+    , { src: "../../icon/upload/avatar_14.png"}
+    , { src: "../../icon/upload/avatar_15.png"}
+    , { src: "../../icon/upload/avatar_16.png"}
+    , { src: "../../icon/upload/avatar_9.png"}],
+    icon_cell:"../../icon/head.svg",
+    icon_share: "../../icon/zhuan.svg",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
